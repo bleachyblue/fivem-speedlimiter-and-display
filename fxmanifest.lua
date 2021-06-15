83a70c8995c1b3ee2ece4d64390ec44e70e29823
bleachyblue <67507150+bleachyblue@users.noreply.github.com>
2021-06-15 22:02:57 +0100

    Updated from __resource.lua to fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+version '1.0.1'
+author 'bleachy#1960'
+description 'A speed limit display and automatic speed limit script'
+repository 'https://github.com/bleachyblue/fivem-speedlimiter-and-display'
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
+
+fx_version 'adamant'
+game 'gta5'
