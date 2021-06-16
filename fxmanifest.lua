8c3df6ae5975c7d94354e369e4cdc45ce2566123
bleachyblue <67507150+bleachyblue@users.noreply.github.com>
2021-06-16 09:22:54 +0100

    Increment version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-version '1.0.1'
+version '1.0.2'
 author 'bleachy#1960'
 description 'A speed limit display and automatic speed limit script'
 repository 'https://github.com/bleachyblue/fivem-speedlimiter-and-display'
